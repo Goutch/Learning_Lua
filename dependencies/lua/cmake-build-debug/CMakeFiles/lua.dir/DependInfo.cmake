edd89c42470b4526193ba3884617646c12656629
Gabriel Bouchard <gaboutch@outlook.com>
2021-01-08 16:32:10 -0500

    changes

---
+++ dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lapi.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lapi.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lauxlib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lauxlib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lbaselib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lbaselib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lbitlib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lbitlib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lcode.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lcode.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lcorolib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lcorolib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lctype.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lctype.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/ldblib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/ldblib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/ldebug.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/ldebug.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/ldo.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/ldo.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/ldump.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/ldump.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lfunc.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lfunc.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lgc.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lgc.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/linit.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/linit.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/liolib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/liolib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/llex.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/llex.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lmathlib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lmathlib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lmem.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lmem.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/loadlib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/loadlib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lobject.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lobject.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lopcodes.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lopcodes.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/loslib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/loslib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lparser.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lparser.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lstate.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lstate.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lstring.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lstring.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lstrlib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lstrlib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/ltable.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/ltable.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/ltablib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/ltablib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/ltm.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/ltm.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lua.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lua.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/luac.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/luac.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lundump.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lundump.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lutf8lib.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lutf8lib.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lvm.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lvm.c.obj"
+  "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/src/lzio.c" "C:/Users/User/Documents/GitHub/Learning_Lua/dependencies/lua/cmake-build-debug/CMakeFiles/lua.dir/src/lzio.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
